40f05d68a60505b978b02031e58d98a9d75a9b8d
Temmy <abdulkareemtemitope01@gmail.com>
2024-09-01 22:14:29 +0100

    first commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+name = input('Your name: ')
+age = input('Your age: ')
+location = input('Your location: ')
+
+print('Hello', name, ',', 'you are', age, 'years old', 'and you live in', location)
+# print('Data type of', type(name))
+
+# print('You are', age,)
+# print('Data type of', type(age))
+
+# print('And you live in', location)
+# print('Data type of', type(location))
